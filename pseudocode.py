edebf5170c362a8937c489da9cd054f3e4f1ab95
Sean <smitchell36@csu.fullerton.edu>
2021-06-24 22:19:00 -0700

    pseudo update

--- pseudocode.py
+++ pseudocode.py
@@ -4,7 +4,8 @@ def tribonacci_recursive(n):
     elif n == 2:
         return 1
     else:
-        return tribonacci_recursive(n-3) +tribonacci_recursive(n-1) + tribonacci_recursive(n-2)
+        return tribonacci_recursive(n - 3) + tribonacci_recursive(n - 1) + tribonacci_recursive(n - 2)
+
 
 def tribonacci_iterative(n):
     if n == 0 or n == 1:
@@ -14,25 +15,33 @@ def tribonacci_iterative(n):
     n3 = 0
     n2 = 1
     n1 = 1
-    trib = n3+n2+n1
-    for i in range(3,n-1):
+    trib = n3 + n2 + n1
+    for i in range(3, n - 1):
         n3 = n2
         n2 = n1
         n1 = trib
         trib = n3 + n2 + n1
     return trib
 
+
+
 def tribonacci_memoized(n):
-    # TODO - implement me
+    C = [None] * max(n + 1, 3)
+    tribonacci_memoized_helper(n, C)
     return 0
 
+def tribonacci_memoized_helper(n, C):
+    if C[n] is not None:
+        return C[n]
+
+
 def tribonacci_dynamic(n):
-    A = [None] * max(n+1,3)
+    A = [None] * max(n + 1, 3)
     A[0] = 0
     A[1] = 0
     A[2] = 1
-    for i in range(3, n+1):
-        A[i] = A[i-3] + A[i-2] + A[i-1]
+    for i in range(3, n + 1):
+        A[i] = A[i - 3] + A[i - 2] + A[i - 1]
     return A[n]
 
 # Test functions here
