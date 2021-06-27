5965184936226e0039f4df4598bc26e993c1bf9d
Slimsloth <toomanyseans@gmail.com>
2021-06-26 23:47:42 -0700

    comments

--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,11 @@
 #include "header.h"
-
+/*******************************************************************
+ * TRIBONACCI RECURSIVE                                            *
+ *******************************************************************
+ * Recursive method of the tribonacci funcion.                     *
+ * Input: int n > 0                                                *
+ * Output: The nth element of the tribonacci sequence.             *
+ *******************************************************************/
 int tribonacci_recursive(int n) {
   if (n == 0 || n == 1) {
     return 0;
@@ -11,6 +17,13 @@ int tribonacci_recursive(int n) {
   }
 }
 
+/*******************************************************************
+ * TRIBONACCI ITERATIVE                                            *
+ *******************************************************************
+ * Iterative method of the tribonacci funcion.                     *
+ * Input: int n > 0                                                *
+ * Output: The nth element of the tribonacci sequence.             *
+ *******************************************************************/
 int tribonacci_iterative(int n) {
   if (n == 0 || n == 1) {
     return 0;
@@ -30,13 +43,16 @@ int tribonacci_iterative(int n) {
   return trib;
 }
 
+/*******************************************************************
+ * TRIBONACCI MEMOIZED                                             *
+ *******************************************************************
+ * Memoized method of the tribonacci funcion.                      *
+ * Input: int n > 0                                                *
+ * Output: The nth element of the tribonacci sequence.             *
+ *******************************************************************/
 int tribonacci_memoized(int n) {
   int max;
-  if (n + 1 > 3) {
-    max = n + 1;
-  } else {
-    max = 3;
-  }
+  (n + 1 > 3) ? max = n + 1 : max = 3;
   std::vector<int> C(max, 2147483647);
   C[0] = 0;
   C[1] = 0;
@@ -45,24 +61,37 @@ int tribonacci_memoized(int n) {
   return trib;
 }
 
+/*******************************************************************
+ * TRIBONACCI MEMOIZED HELPER                                      *
+ *******************************************************************
+ * Helper function for tribnacci_memoized.                         *
+ * Passed a vector by reference for top-down solution list.        *
+ * Input: int n > 0, vector<int> &C                                *
+ * Output: The nth element of the tribonacci sequence.             *
+ *******************************************************************/
 int tribonacci_memoized_helper(int n, std::vector<int> &C) {
   int result;
   if (C[n] != 2147483647) {
     return C[n];
   } else {
-    result = tribonacci_memoized_helper(n-3, C) + tribonacci_memoized_helper(n-2, C) + tribonacci_memoized_helper(n-1, C);
+    result = tribonacci_memoized_helper(n - 3, C) +
+             tribonacci_memoized_helper(n - 2, C) +
+             tribonacci_memoized_helper(n - 1, C);
   }
   C[n] = result;
   return result;
 }
 
+/*******************************************************************
+ * TRIBONACCI DYNAMIC                                              *
+ *******************************************************************
+ * Dynamic method of the tribonacci funcion.                       *
+ * Input: int n > 0                                                *
+ * Output: The nth element of the tribonacci sequence.             *
+ *******************************************************************/
 int tribonacci_dynamic(int n) {
   int max;
-  if (n+1> 3) {
-    max = n + 1;
-  } else {
-    max = 3;
-  }
+  (n + 1 > 3) ? max = n + 1 : max = 3;
   std::vector<int> A(max, 2147483647);
   A[0] = 0;
   A[1] = 0;
--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "header.h"
 
 int main() {
+  /*******************************************************************
+   * CPSC 335 Project 4 Test Driver Code                             *
+   *******************************************************************
+   * Group Members: Sean Mitchell, Jason Mora-Mendoza.               *
+   * This project implements the tribonacci algorithm through 4      *
+   * different methods: recursively, iteratively, memoized, and      *
+   * dynamically.                                                    *
+   *******************************************************************/
   std::cout << "\nRECURSIVE METHOD\n";
   for (int i = 0; i < 10; i++) {
     std::cout << tribonacci_recursive(i) << ", ";
   }
   std::cout << "\nITERATIVE METHOD\n";
   for (int i = 0; i < 10; i++) {
-      std::cout << tribonacci_iterative(i) << ", ";
+    std::cout << tribonacci_iterative(i) << ", ";
   }
   std::cout << "\nMEMOIZED METHOD\n";
   for (int i = 0; i < 10; i++) {
@@ -17,5 +25,6 @@ int main() {
   for (int i = 0; i < 10; i++) {
     std::cout << tribonacci_dynamic(i) << ", ";
   }
+  std::cout << "\n\nEnd of output\n\n";
   return 0;
 }
